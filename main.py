6f58c3b2d4de0f4cbdd322e61c10a5e8fc0d8234
ronnygranados <ronnygranados7@gmail.com>
2022-01-08 15:16:37 -0600

    object file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 import random
-from object import Object
+# from object_file import Object
 # from pygame.math import Vector2
 
 pygame.init()
@@ -47,6 +47,29 @@ class Figure:
         SCREEN.blit(DITTO, (mouse_x - 90, 330))
 
 
+class Object:
+    def __init__(self):
+        self.red_surface = pygame.Surface((50, 50))
+        self.red_surface.fill(RED)
+        self.green_surface = pygame.Surface((50, 50))
+        self.green_surface.fill(GREEN)
+
+        self.x = random_cell * CELL_SIZE
+        self.y = -100
+
+        self.red_rect = self.red_surface.get_rect(center=(self.x, self.y))
+        self.green_rect = self.green_surface.get_rect(center=(100, self.y))
+        # self.pos = Vector2(self.x, self.y)
+
+    def blit_shape(self):
+        SCREEN.blit(self.red_surface, self.red_rect)
+        SCREEN.blit(self.green_surface, self.green_rect)
+
+    def fall_shape(self):
+        self.red_rect.y += 2
+        self.green_rect.y += 2
+
+
 class Main:
     def __init__(self):
         self.object = Object()
@@ -61,14 +84,17 @@ SCREEN_UPDATE = pygame.USEREVENT
 figure = Figure()
 object_class = Object()
 
+mx, my = SCREEN.get_rect().center
+
 running = True
 while running:
     CLOCK.tick(FPS)
-    mx, my = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            running = False
+
+    if pygame.mouse.get_focused():
+        mx, my = pygame.mouse.get_pos()
 
     SCREEN.fill(LIGHT_GREEN)
     SCREEN.blit(floor_surface, floor_rect)
@@ -76,3 +102,6 @@ while running:
     object_class.blit_shape()
     object_class.fall_shape()
     pygame.display.update()
+
+pygame.quit()
+sys.exit()
